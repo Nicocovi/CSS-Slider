66de846bd3adfcd68e6f0aa66ed58b97ff6e8759
Nicocovi <nico0205@hotmail.com>
2017-10-24 15:54:32 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var firstClick = function(){
+	var container = document.getElementById("container");
+	container.className = "container two slider-two-active";
+}
+
+var secondClick = function(){
+	var container = document.getElementById("container");
+	container.className = "container three slider-three-active";
+}
+
+var thirdClick = function(){
+	var container = document.getElementById("container");
+	container.className = "container four slider-four-active";
+}
+	
+var fourthClick = function(){
+	var container = document.getElementById("container");
+	container.className = "container five slider-five-active";
+}
